4b24e26f52886f5a6eda98457bbe2ab6c4d5af12
Jimmy Mathew <jimmymathew765@gmail.com>
2025-02-18 23:17:24 +0530

    Delete learn.py

--- learn.py
+++
@@ -1,9 +0,0 @@
-st=input("Enter the string:")
-key= int(input("Enter the key:"))
-enst=""
-for ch in st:
-    ordch=ord(ch)+key
-    if(ordch>ord("z")):
-       ordch= ord("a")+(ordch-ord("z")-1)
-    enst+=chr(ordch)
-print(enst)
